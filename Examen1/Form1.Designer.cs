f75964e7985a615cd9f8445f641fd1a17de02a21
Elisis-5002 <ana.elisa.bailon@gmail.com>
2026-02-26 15:52:50 -0600

    Examen Unidad1

--- Examen1/Form1.Designer.cs
+++ Examen1/Form1.Designer.cs
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            txSueldoInicial = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            btnCalcular = new Button();
+            lblISR = new Label();
+            lblSueldoFinal = new Label();
+            SuspendLayout();
+            // 
+            // txSueldoInicial
+            // 
+            txSueldoInicial.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txSueldoInicial.Location = new Point(575, 47);
+            txSueldoInicial.Name = "txSueldoInicial";
+            txSueldoInicial.Size = new Size(199, 45);
+            txSueldoInicial.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(39, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(226, 38);
+            label1.TabIndex = 1;
+            label1.Text = "Sueldo Mensual";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(39, 139);
+            label2.Name = "label2";
+            label2.Size = new Size(191, 38);
+            label2.TabIndex = 3;
+            label2.Text = "ISR calculado";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(39, 229);
+            label3.Name = "label3";
+            label3.Size = new Size(392, 38);
+            label3.TabIndex = 5;
+            label3.Text = "Sueldo Neto después del ISR";
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCalcular.Location = new Point(39, 324);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(194, 56);
+            btnCalcular.TabIndex = 6;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
+            // 
+            // lblISR
+            // 
+            lblISR.BackColor = SystemColors.ButtonHighlight;
+            lblISR.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblISR.Location = new Point(575, 139);
+            lblISR.Name = "lblISR";
+            lblISR.Size = new Size(199, 38);
+            lblISR.TabIndex = 7;
+            // 
+            // lblSueldoFinal
+            // 
+            lblSueldoFinal.BackColor = SystemColors.ButtonHighlight;
+            lblSueldoFinal.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblSueldoFinal.Location = new Point(575, 220);
+            lblSueldoFinal.Name = "lblSueldoFinal";
+            lblSueldoFinal.Size = new Size(199, 38);
+            lblSueldoFinal.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Controls.Add(lblSueldoFinal);
+            Controls.Add(lblISR);
+            Controls.Add(btnCalcular);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txSueldoInicial);
+            Name = "Form1";
+            Text = "Calculo del ISR";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txSueldoInicial;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txSueldoFinal;
+        private Button btnCalcular;
+        private Label lblISR;
+        private Label lblSueldoFinal;
     }
 }
--- Examen1/Form1.cs
+++ Examen1/Form1.cs
@@ -2,9 +2,105 @@ namespace Examen1
 {
     public partial class Form1 : Form
     {
+        double sueldo;
+        double excedente, isr, sueldoFinal;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblISR.Text = "";
+                lblSueldoFinal.Text = "";
+
+                sueldo = double.Parse(txSueldoInicial.Text);
+                if (sueldo < 0)
+                {
+                    MessageBox.Show("El sueldo no puede ser negativo.");
+                    return;
+                }
+                if (0.01 < sueldo && sueldo < 746.04)
+                {
+                    excedente = sueldo - 0.01;
+                    isr = excedente * 0.0192;
+                    sueldoFinal = sueldo - isr;
+                } 
+                if (746.05 < sueldo && sueldo < 6332.05)
+                {
+                    excedente = sueldo - 746.05;
+                    isr = 14.32 + (excedente * 0.0640);
+                    sueldoFinal = sueldo - isr;
+                } 
+                if (6332.06 < sueldo && sueldo < 11128.01)
+                {
+                    excedente = sueldo - 6332.06;
+                    isr = 371.83 + (excedente * 0.1088);
+                    sueldoFinal = sueldo - isr;
+                }
+                if (11128.02 < sueldo && sueldo < 12935.82)
+                {
+                    excedente = sueldo - 11128.02;
+                    isr = 893.63 + (excedente * 0.16);
+                    sueldoFinal = sueldo - isr;
+                }
+                if (12935.83 < sueldo && sueldo < 15487.71)
+                {
+                    excedente = sueldo - 12935.83;
+                    isr = 1318.26 + (excedente * 0.1792);
+                    sueldoFinal = sueldo - isr;
+                }
+                if (15487.72 < sueldo && sueldo < 31236.49)
+                {
+                    excedente = sueldo - 15487.72;
+                    isr = 1972.12 + (excedente * 0.2136);
+                    sueldoFinal = sueldo - isr;
+                }
+                if (31236.50 < sueldo && sueldo < 49233.00)
+                {
+                    excedente = sueldo - 31236.50;
+                    isr = 5850.76 + (excedente * 0.2352);
+                    sueldoFinal = sueldo - isr;
+                }
+                if (49233.01 < sueldo && sueldo < 93993.90)
+                {
+                    excedente = sueldo - 49233.01;
+                    isr = 10837.02 + (excedente * 0.30);
+                    sueldoFinal = sueldo - isr;
+                }
+                if (93993.91 < sueldo && sueldo < 125325.20)
+                {
+                    excedente = sueldo - 93993.91;
+                    isr = 26177.04 + (excedente * 0.32);
+                    sueldoFinal = sueldo - isr;
+                }
+                if (125325.21 < sueldo && sueldo < 375975.61)
+                {
+                    excedente = sueldo - 125325.21;
+                    isr = 38243.68 + (excedente * 0.34);
+                    sueldoFinal = sueldo - isr;
+                }
+                if (375975.62 < sueldo)
+                {
+                    excedente = sueldo - 375975.62;
+                    isr = 142119.38 + (excedente * 0.35);
+                    sueldoFinal = sueldo - isr;
+                }
+                
+                lblISR.Text = isr.ToString("F2");
+                lblSueldoFinal.Text = sueldoFinal.ToString("F2");
+
+
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Ingrese un número válido para el sueldo.");
+                return;
+            }
         }
     }
 }
